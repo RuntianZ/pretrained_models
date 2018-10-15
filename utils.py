6f0bd46652b7407a09b3308380c4323781fc3253
Ubuntu <ubuntu@ip-172-31-0-73.us-east-2.compute.internal>
2018-10-15 01:10:49 +0000

    clean

--- utils.py
+++ utils.py
@@ -4,25 +4,23 @@ from __future__ import print_function
 
 import os
 from six.moves import urllib
-import tensorflow as tf
 
 
 def download(directory, filename, source_url):
-  """Download (and unzip) a file from the MNIST dataset if not already done."""
   filepath = os.path.join(directory, filename)
-  if tf.gfile.Exists(filepath):
+  if os.path.isfile(filepath):
     return filepath
-  if not tf.gfile.Exists(directory):
-    tf.gfile.MakeDirs(directory)
-  url = source_url + filename
+  if not os.path.isdir(directory):
+    os.makedirs(directory)
+  url = source_url.format(filename)
   print('Downloading %s to %s' % (url, filepath))
   urllib.request.urlretrieve(url, filepath)
   return filepath
 
 
-def merge_file(fromdir, todir, filename):
+def merge(fromdir, todir, filename):
   if not os.path.exists(todir):
-    os.mkdir(todir)
+    os.makedirs(todir)
   else:
     for fname in os.listdir(todir):
       os.remove(os.path.join(todir, fname))
@@ -41,5 +39,28 @@ def merge_file(fromdir, todir, filename):
         fout.write(chunk)
       partnum += 1
     partnum -= 1
+
+  return partnum
+
+
+def split(fromdir, todir, filename, chunksize=20000000):
+  if not os.path.exists(todir):
+    os.mkdir(todir)
+  else:
+    for fname in os.listdir(todir):
+      os.remove(os.path.join(todir, fname))
+
+  fromfile = os.path.join(fromdir, filename)
+  partnum = 0
+  with open(fromfile, 'rb') as fin:
+    while True:
+      chunk = fin.read(chunksize)
+      if not chunk:
+        break
+
+      partnum += 1
+      fname = os.path.join(todir, ('{}.part{}'.format(filename, partnum)))
+      with open(fname, 'wb') as fout:
+        fout.write(chunk)
 
   return partnum
